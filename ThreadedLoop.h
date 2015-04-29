dc9691d707e0a0fcac008845eb7433869d21ce78
unknown <b010805a@k106-20.student.staffs.ac.uk>
2015-04-29 13:50:16 +0100

    Updated ThreadedLoop

--- ThreadedLoop.h
+++ ThreadedLoop.h
@@ -14,9 +14,9 @@
 	#include <Windows.h>
 #endif
 
-class ThreadedLoop
+template <class T> class ThreadedLoop
 {
-	typedef std::function<void(int, int, int)> ThreadedLoopFunction;
+	typedef std::function<void(T* start, T* end)> ThreadedLoopFunction;
 
 	ThreadedLoopFunction func;
 
@@ -31,23 +31,34 @@ class ThreadedLoop
 	std::vector<std::thread> threads;
 	std::condition_variable mainConditionalVariable;
 
+	std::vector<T>* data = nullptr;
+
 public:
 
 	//Disable copying to control the thread-creation
 	ThreadedLoop(ThreadedLoop& a) = delete;
 
-	ThreadedLoop(ThreadedLoopFunction func, int totalBoids)
+	ThreadedLoop(ThreadedLoopFunction func, std::vector<T>* data = nullptr)
 	{
+		this->data = data;
+
+		if (!this->data)
+		{
+			assert(false);
+		}
+
 		memset((void*)this->running, 0,  sizeof(bool) * THREAD_COUNT);
 
 		for (int i = 0; i < THREAD_COUNT; i++)
 		{
 			threads.push_back(std::thread([=]()
 			{
+
 #ifdef WIN32
 				SetThreadAffinityMask(GetCurrentThread(), 1 << i);
 				SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST);
 #endif
+
 				while (true)
 				{
 					std::unique_lock<std::mutex> lck(mutexie[i]);
@@ -63,10 +74,18 @@ public:
 						return;
 					}
 					
+					auto totalBoids = data->size();
+
 					auto perThread = (totalBoids / THREAD_COUNT);
 
 					//If we're the final thread, add the remainder to the loop to finish all the boids
-					func(i, perThread + (i != (THREAD_COUNT - 1) ? 0 : (totalBoids % THREAD_COUNT)), perThread * i);
+					auto perThreadWithRemainder = perThread + (i != (THREAD_COUNT - 1) ? 0 : (totalBoids % THREAD_COUNT));
+					auto offset = perThread * i;
+
+					auto start = (T*)data->data() + offset;
+					auto end = start + perThreadWithRemainder;
+
+					func(start, end);
 
 					finished++;
 					this->running[i] = false;
